05ccc149417743d2d54e96d1c816e2f3beae815b
mygit <mail.me.neal@gmail.com>
2021-11-18 14:50:38 +0100

    first commit

---
+++ app/src/main/java/com/huawei/junction21/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.huawei.junction21
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.coroutineScope
+import com.huawei.hackzurich.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title = getString(R.string.app_title)
+
+        val checkStatusTextView = findViewById<TextView>(R.id.main_check)
+
+        lifecycle.coroutineScope.launchWhenCreated {
+            try {
+                checkHMS()
+                checkStatusTextView.text = getString(R.string.checking_setup_result_ok)
+            } catch (checkException: Exception) {
+                checkStatusTextView.text =
+                    getString(R.string.checking_setup_result_fail, checkException.message)
+            }
+        }
+    }
+
+    private suspend fun checkHMS() {
+        testHmsCorePresence()
+        testAccountByRequestingPushNotificationsToken()
+    }
+
+    private suspend fun testAccountByRequestingPushNotificationsToken() {
+        val pushToken = withContext(Dispatchers.IO) {
+            HmsUtils.getPushNotificationsToken(this@MainActivity)
+        }
+        check(pushToken.isNotEmpty()) { "Push notifications token retrieved, but empty. Clear app data and try again." }
+    }
+
+    private fun testHmsCorePresence() {
+        check(HmsUtils.isHmsAvailable(this)) { "Please make sure you have HMS Core installed on the test device." }
+    }
+}
+
